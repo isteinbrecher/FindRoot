3c3d08a27f1550f2638fe9e689c7b2881979f881
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2022-07-13 18:26:50 +0200

    Working for 2x2

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
 
 #include "lib/newton.H"
-
+#include <iostream>
 #include <array>
 #include <math.h>
 
 struct meins
 {
-  static constexpr unsigned short n_dim = 1;
+  static constexpr unsigned short n_dim = 2;
 
   static void eval_f_jac(const double x[n_dim], double f[n_dim], double jac[n_dim * n_dim])
   {
-    f[0] = std::cos(x[0]);
-    jac[0] = -std::sin(x[0]);
+    f[0] = x[0] * x[0] + x[1] + 2.0;
+    f[1] = 6.0 * x[0] - x[1] * (x[0] + 1.0);
+    jac[0] = 2.0 * x[0];
+    jac[1] = 6.0 - x[1];
+    jac[2] = 1.0;
+    jac[3] = -1.0 - x[0];
   }
 };
 
@@ -19,15 +23,17 @@ struct meins
 int main(int, char*[])
 {
   FindRoot::IterationParameters params;
-  params.max_iterations = 10;
+  params.max_iterations = 100;
 
 
-  std::array<double, 1> x;
-  x[0] = 2;
+  std::array<double, 2> x;
+  x[0] = 5;
+  x[1] = 1;
   FindRoot::IterationData data = FindRoot::NewtonRaphson<meins>(params, x.data());
 
 
   std::cout << "\nx: " << x[0] << "\n";
+  std::cout << "\nx: " << x[1] << "\n";
   std::cout << "\nError: " << data.error << "\n";
   std::cout << "\nNumber of iterations: " << data.iterations << "\n";
   std::cout << "\nConverged: " << data.converged << "\n";
