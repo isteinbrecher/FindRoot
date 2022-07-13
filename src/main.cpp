f598218529cff8ec5b184cc0b027372983af3dd0
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2022-07-13 16:47:09 +0200

    Add parameters

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,28 @@ struct meins
 
   static void eval_f_jac(const double x[n_dim], double f[n_dim], double jac[n_dim * n_dim])
   {
-    f[0] = std::sin(x[0]);
-    jac[0] = std::cos(x[0]);
+    f[0] = std::cos(x[0]);
+    jac[0] = -std::sin(x[0]);
   }
 };
 
 template <>
 struct newton::function<meins>
 {
-  static inline bool check_convergence() { return false; }
+  static inline bool check_convergence() { return true; }
 };
 
 
 
 int main(int argc, char* argv[])
 {
+  newton::newton_data params;
+  params.local_newton_iter_max = 2;
+
+
   std::array<double, 1> x;
   x[0] = 2;
-  newton::newton<meins, 1>(x.data());
+  newton::newton<meins>(params, x.data());
 
 
   std::cout << "\nx: " << x[0] << "\n";
