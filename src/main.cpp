c9d4aa6f3addb26b285ade52c5285b529e1e0dfd
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2022-07-14 10:18:11 +0200

    Add 3x3

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,25 @@
 
 struct meins
 {
-  static constexpr unsigned short n_dim = 2;
+  static constexpr unsigned short n_dim = 3;
 
   static void eval_f_jac(const double x[n_dim], double f[n_dim], double jac[n_dim * n_dim])
   {
-    f[0] = x[0] * x[0] + x[1] + 2.0;
-    f[1] = 6.0 * x[0] - x[1] * (x[0] + 1.0);
-    jac[0] = 2.0 * x[0];
-    jac[1] = 6.0 - x[1];
+    f[0] = x[0] * x[1] - x[1] * x[2] + 1.0;
+    f[1] = x[0] * x[1] * x[2] - 5.0;
+    f[2] = x[0] + x[2] - 2;
+
+    jac[0] = x[1];
+    jac[1] = x[1] * x[2];
     jac[2] = 1.0;
-    jac[3] = -1.0 - x[0];
+
+    jac[3] = x[0] - x[2];
+    jac[4] = x[0] * x[2];
+    jac[5] = 0.0;
+
+    jac[6] = -x[1];
+    jac[7] = x[0] * x[1];
+    jac[8] = 1.0;
   }
 };
 
@@ -27,13 +36,15 @@ int main(int, char*[])
 
 
   std::array<double, 2> x;
-  x[0] = 5;
-  x[1] = 1;
+  x[0] = 1.0;
+  x[1] = 1.0;
+  x[2] = 1.0;
   FindRoot::IterationData data = FindRoot::NewtonRaphson<meins>(params, x.data());
 
 
   std::cout << "\nx: " << x[0] << "\n";
   std::cout << "\nx: " << x[1] << "\n";
+  std::cout << "\nx: " << x[2] << "\n";
   std::cout << "\nError: " << data.error << "\n";
   std::cout << "\nNumber of iterations: " << data.iterations << "\n";
   std::cout << "\nConverged: " << data.converged << "\n";
