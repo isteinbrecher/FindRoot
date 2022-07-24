c268971777a5be20ef6dd3b5be40978d27208942
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2022-07-24 18:54:28 +0200

    Add testing for 2x2

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -8,26 +8,39 @@
 
 constexpr double tol = 1e-12;
 
+
+struct Function2x2
+{
+  static constexpr unsigned short n_dim = 2;
+
+  static void eval_f_jac(const double x[n_dim], double f[n_dim], double jac[n_dim * n_dim])
+  {
+    f[0] = x[0] * x[0] + x[1] + 2.0;
+    f[1] = 6.0 * x[0] - x[1] * (x[0] + 1.0);
+    jac[0] = 2.0 * x[0];
+    jac[1] = 6.0 - x[1];
+    jac[2] = 1.0;
+    jac[3] = -1.0 - x[0];
+  }
+};
+
+
 /**
  * \brief
  */
 TEST(nr_tests, test_2x2)
 {
-  // const std::vector<char> input = {0, 16, -125, 16, 81, -121, 32, -110, -117, 48, -45, -113, 65,
-  // 20,
-  //     -109, 81, 85, -105, 97, -106, -101, 113, -41, -97, -126, 24, -93, -110, 89, -89, -94, -102,
-  //     -85, -78, -37, -81, -61, 28, -77, -45, 93, -73, -29, -98, -69, -13, -33, -65, 0, 16, -125,
-  //     16, 81, -121, 32, -110, -117, 48, -45, -113, 65, 20, -109, 81, 85, -105, 97, -106, -101,
-  //     113, -41, -97, -126, 24, -93, -110, 89, -89, -94, -102, -85, -78, -37, -81, -61, 28, -77,
-  //     -45, 93, -73, -29, -98, -69, -13, -33, -65};
-  // const auto encoded = base64::encode(input.data(), input.size());
-  // EXPECT_EQ(
-  //     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-  //     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
-  //     encoded);
-  // const auto decoded = base64::decode(encoded);
-  // EXPECT_EQ(input, decoded);
+  FindRoot::IterationParameters params;
+
+  std::array<double, 2> x;
+  x[0] = 5.0;
+  x[1] = 1.0;
+  FindRoot::IterationData data = FindRoot::NewtonRaphson<Function2x2>(params, x.data());
+
+  EXPECT_NEAR(x[0], -0.25224642002463515, tol);
+  EXPECT_NEAR(x[1], -2.0632453402062128, tol);
 }
+
 
 struct Function3x3
 {
