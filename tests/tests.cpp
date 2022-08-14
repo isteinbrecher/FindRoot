3e7a63f092a1834d0dcc61cdbe21e2758d691e36
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2022-08-14 18:03:31 +0200

    Add performance tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -270,3 +270,37 @@ void Test3x3()
  */
 TEST(nr_tests, test_3x3) { Test3x3<Function3x3>(); }
 TEST(nr_tests, test_3x3FortranType) { Test3x3<Function3x3FortranType>(); }
+
+
+
+#include <chrono>
+#include <random>
+
+void PerformanceTest3x3(const unsigned int factor, const double expected_time)
+{
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(-0.2, 0.2);
+  std::array<double, 3> x;
+
+  auto time_begin = std::chrono::system_clock::now();
+
+  for (unsigned int i = 0; i < factor; i++)
+  {
+    FindRoot::IterationParameters params;
+
+    x[0] = 1.0 + dis(gen);
+    x[1] = 1.0 + dis(gen);
+    x[2] = 1.0 + dis(gen);
+    FindRoot::IterationData data = FindRoot::NewtonRaphson<Function3x3>(params, x);
+
+    EXPECT_NEAR(x[0], 0.90098048640721518, tol);
+    EXPECT_NEAR(x[1], 5.0495097567963922, tol);
+    EXPECT_NEAR(x[2], 1.0990195135927849, tol);
+  }
+
+  const std::chrono::duration<double> duration = std::chrono::system_clock::now() - time_begin;
+  EXPECT_LT(duration.count(), expected_time);
+}
+
+TEST(nr_tests, test_3x3Performance) { PerformanceTest3x3(100000, 0.1); }
