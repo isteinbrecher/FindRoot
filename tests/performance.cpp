cdb0bc1bdf0d920ba5d9fc1b50426bd378803e8b
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2022-08-15 18:18:37 +0200

    Add performance tests for 2x2

--- tests/performance.cpp
+++ tests/performance.cpp
@@ -11,28 +11,41 @@
 #include <chrono>
 #include <random>
 
-void PerformanceTest3x3(const unsigned int factor, const double expected_time)
+
+template <typename fun, typename T_init, typename T_check>
+void PerformanceTest(
+    T_init fun_init, T_check fun_check, const unsigned int factor, const double expected_time, const double var = 0.2)
 {
+  constexpr unsigned short n_dim = fun::n_dim;
+
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_real_distribution<> dis(-0.2, 0.2);
-  std::array<double, 3> x;
+  std::uniform_real_distribution<> dis(-var, var);
+  std::array<double, n_dim> x;
   FindRoot::IterationParameters params;
 
   auto time_begin = std::chrono::system_clock::now();
 
   for (unsigned int i = 0; i < factor; i++)
   {
-    init_3x3(x);
-    x[0] += dis(gen);
-    x[1] += dis(gen);
-    x[2] += dis(gen);
-    FindRoot::NewtonRaphson<Function3x3>(params, x);
-    check_solution_3x3(x);
+    fun_init(x);
+    for (unsigned char j = 0; j < n_dim; j++) x[j] += dis(gen);
+    FindRoot::NewtonRaphson<fun>(params, x);
+    fun_check(x);
   }
 
   const std::chrono::duration<double> duration = std::chrono::system_clock::now() - time_begin;
   EXPECT_LT(duration.count(), expected_time);
 }
 
-TEST(nr_tests, test_3x3Performance) { PerformanceTest3x3(100000, 0.1); }
+
+TEST(nr_tests, test_2x2_performance)
+{
+  PerformanceTest<Function2x2>(
+      init_2x2<std::array<double, 2>>, check_solution_2x2_no_iterations<std::array<double, 2>>, 130000, 0.1);
+}
+TEST(nr_tests, test_3x3_performance)
+{
+  PerformanceTest<Function3x3>(
+      init_3x3<std::array<double, 3>>, check_solution_3x3_no_iterations<std::array<double, 3>>, 100000, 0.1);
+}
